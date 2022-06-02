0c8f8d209d5619a8fc4cfef8100a51d7a089d2fe
HsuLayPhyu <hsulayphyu77@gmail.com>
2022-06-02 17:33:34 +0630

    Day1&2

---
+++ Day1/sandbox.js
@@ -0,0 +1,18 @@
+let name = "Hsu Lay";
+
+let age = 24;
+
+
+//log things to console
+
+console.log(name, age);
+
+ name ="Cheryl";
+console.log(name);
+
+const points = 100;
+//points = 150;
+console.log(points);
+
+var year = 1998;
+console.log(year);
---
+++ Day2/sandbox.js
@@ -0,0 +1,48 @@
+let email="hsulay@gmail.com";
+
+//let result = email.lastIndexOf('h');
+
+//let result= email.slice(0,7);
+//let result= email.substr(0,6);
+//let result= email.replace('h','H');
+
+//console.log(result);
+
+let radius= 10;
+const pi= 3.14;
+
+//console.log(radius, pi);
+
+//math operations +,-,*,%,**,/
+
+//console.log(10/2);
+
+//let result = radius % 2;
+//let result = pi* radius**2;
+
+//order of operation - B I D M A S
+
+//let result= 5*(10-2)**2;
+
+//console.log(result);
+
+let likes = 10;
+
+//likes = likes +1;
+
+//likes++;
+//likes--;
+
+//likes+= 10;
+//likes-= 10;
+//likes*= 10;
+//likes/= 10;
+
+//console.log(likes);
+
+//NaN - not a number
+
+//console.log(5/ 'hola');
+
+let result = 'the blog has' + ' ' + likes +' '+ 'likes';
+console.log(result);
