432882af5015a8f54ae445072c55f1c5683fada8
HsuLayPhyu <hsulayphyu77@gmail.com>
2022-06-29 14:35:41 +0630

    Day23,24,25

--- weather/scripts/app.js
+++ weather/scripts/app.js
@@ -1,13 +1,19 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+    //console.log(data);
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
 
-    //update template
+    //destructure properties
+    const{cityDets,weather} = data;
+
+    //update details template
     details.innerHTML = ` 
         <h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
@@ -16,6 +22,20 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    //update the night/day & icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src',iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // }
+     time.setAttribute('src', timeSrc);
+
     //remove d-none class if present
     if(card.classList.contains('d-none')){
         card.classList.remove('d-none');
@@ -44,3 +64,6 @@ cityForm.addEventListener('submit', e => {
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
+
+// const result = true ? 'value 1' : 'value 2';
+// console.log(result);
