3158e0c67c81e302d699334159756ef1637afd83
HsuLayPhyu <hsulayphyu77@gmail.com>
2022-07-19 13:14:06 +0630

    Day36,37,38,39

--- babel_webpack/webpack.config.js
+++ babel_webpack/webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         publicPath: '/assets/'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            user: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
     }
 
 };
